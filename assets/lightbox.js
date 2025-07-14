91759fc2b837ccb0b185a72de747d71e42513f3f
hydraulic-mining-excavator <166040961+hydraulic-mining-excavator@users.noreply.github.com>
2025-07-14 13:03:37 +0300

    Update lightbox.js

--- assets/lightbox.js
+++ assets/lightbox.js
@@ -111,7 +111,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if(event.target.id != 'next' && event.target.id != 'prev'){
             this.innerHTML = '';
             document.getElementById('lightbox').style.display = 'none';
-	    document.body.style.overflow = document.body.style.overflow == 'hidden' ? 'auto' : 'hidden';
+	    document.body.style.overflow = 'auto';
         }
     });
     
@@ -134,7 +134,7 @@ document.addEventListener("DOMContentLoaded", function() {
             event.preventDefault();
             document.getElementById('lightbox').innerHTML = '<a id="close"></a><a id="next">&rsaquo;</a><a id="prev">&lsaquo;</a><div class="img" style="background: url(\''+this.getAttribute('href')+'\') center center / contain no-repeat;" title="'+this.getAttribute('title')+'" ><img src="'+this.getAttribute('href')+'" alt="'+this.getAttribute('title')+'" /></div><span>'+this.getAttribute('title')+'</span>';
             document.getElementById('lightbox').style.display = 'block';
-	    document.body.style.overflow = document.body.style.overflow == 'hidden' ? 'auto' : 'hidden';
+	    document.body.style.overflow = 'hidden';
 
             setGallery(this);
         });
